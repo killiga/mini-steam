6453fef1eff54e028b5f8a21037befba7f4a1fd0
Lux <mainlux01011@gmail.com>
2026-07-20 17:55:12 +0300

    add sort

--- store/views.py
+++ store/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Game, Purchase
 from django.contrib.auth.decorators import login_required
 
+
 def games(request):
 
     search = request.GET.get("search")
+    sort = request.GET.get("sort")
 
     if search:
         games = Game.objects.filter(
-            name__icontains=search
+            name__icontains=search  
         )
     else:
         games = Game.objects.all()
+
+
+    if sort == "price_asc":
+        games = games.order_by("price")
+
+    elif sort == "price_desc":
+        games = games.order_by("-price")
+
+    elif sort == "date":
+        games = games.order_by("-release_date")
+
 
     return render(request, "store/games.html", {
         "games": games,
